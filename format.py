abab1d51e85bb00395121b7b50e899cc692c65e1
Dev <codekidinprogress@gmail.com>
2025-12-18 22:48:39 +0530

    Add

---
+++ format.py
@@ -0,0 +1,6 @@
+print("{:<10} {:<10} {:<10}".format("Name", "Age", "City"))
+print("-" * 30)
+
+print("{:<10} {:<10} {:<10}".format("Noah", 20, "New York"))
+print("{:<10} {:<10} {:<10}".format("Kaneki", 25, "Tokyo"))
+print("{:<10} {:<10} {:<10}".format("Jamie", 22, "LA"))
