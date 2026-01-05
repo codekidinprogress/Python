31468c85473e4feb7961b65a91a7bd196922793a
Dev <codekidinprogress@gmail.com>
2026-01-05 22:59:02 +0530

    Add Decorator program

--- Decorator.py
+++ Decorator.py
@@ -1,10 +1,16 @@
-def my_decorator(func):
-    def wrapper():
-        print("Before function")
-        func()
-        print("After function")
+def log_discount(func):
+    def wrapper(amount):
+        print("Applying discount...")
+        result = func(amount)
+        print("Discount applied successfully!")
+        return result
     return wrapper
-@my_decorator
-def say_hi():
-    print("Hi!")    
-say_hi()
+
+@log_discount
+def apply_discount(amount):
+    discount = amount * 0.10   # 10% discount
+    final_price = amount - discount
+    print("Final Price:", final_price)
+    return final_price
+
+apply_discount(2000)
